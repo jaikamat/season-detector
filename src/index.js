9d7308c69821ad40b783952eb84029e5bd671fc5
Admin <jaipkamat@gmail.com>
2019-04-05 11:23:57 -0700

    Scaffolding simple app logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => {
-    return "hello world";
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            error => {
+                this.setState({ errorMessage: error.message });
+            }
+        );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading...</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
